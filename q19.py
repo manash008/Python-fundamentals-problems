b52cc0582b88398d804b211c86a1358a7fd087c8
Manish Kumar <manashfrost1808@gmail.com>
2026-02-09 23:52:15 +0530

    Refactor prime number check in q19.py
    Refactored prime number checking logic to improve clarity.

--- q19.py
+++ q19.py
@@ -1,6 +1,8 @@
+#Write a function that returns if is a prime number and otherwise,using a loop
 n=int(input("enter your number"))
 for i in range(2,n):
     if n%i==0:
       print ("is _not prime")
     elif n%i!=0:
      print("is prime")
+
